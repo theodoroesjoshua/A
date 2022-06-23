1c5f186270b482433fad784063acddcd26625e8f
yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2022-06-23 16:45:32 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sugoi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sugoi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "De'Sushi Membership",
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.home), child: Text('Beranda')),
+                Tab(icon: Icon(Icons.history), child: Text('Riwayat')),
+                Tab(icon: Icon(Icons.person), child: Text('Akun')),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
