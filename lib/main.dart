22e8a38e133cf2b3301f8c6142c97a01d9e34f3f
yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2022-06-25 13:40:44 +0800

    Implement basic card list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(MainApp());
+  runApp(const MainApp());
 }
 
 class MainApp extends StatefulWidget {
@@ -14,21 +15,6 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   int _selectedIndex = 0;
 
-  static const List<Widget> _pages = <Widget>[
-    Icon(
-      Icons.call,
-      size: 150,
-    ),
-    Icon(
-      Icons.camera,
-      size: 150,
-    ),
-    Icon(
-      Icons.chat,
-      size: 150,
-    ),
-  ];
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -37,11 +23,25 @@ class _MainAppState extends State<MainApp> {
       home: Scaffold(
         body: IndexedStack(
           index: _selectedIndex,
-          children: _pages,
+          children: _createMainPages(),
         ),
         bottomNavigationBar: _bottomNavigationBar(),
       ),
     );
+  }
+
+  List<Widget> _createMainPages() {
+    return <Widget>[
+      const HomePage(),
+      const Icon(
+        Icons.camera,
+        size: 150,
+      ),
+      const Icon(
+        Icons.chat,
+        size: 150,
+      ),
+    ];
   }
 
   Widget _bottomNavigationBar() {
@@ -69,5 +69,53 @@ class _MainAppState extends State<MainApp> {
     setState(() {
       _selectedIndex = index;
     });
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class Voucher {
+  final String description;
+
+  const Voucher({
+    required this.description,
+  });
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<Voucher> vouchers;
+
+  @override
+  void initState() {
+    super.initState();
+    vouchers = <Voucher>[];
+
+    var rng = Random();
+    final totalDummyVouchers = rng.nextInt(5) + 1;
+    for (var i = 0; i < totalDummyVouchers; i++) {
+      vouchers.add(Voucher(description: "test $i"));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView(
+        padding: const EdgeInsets.all(8),
+        children: vouchers.map(_createVoucherWidget).toList(),
+      ),
+    );
+  }
+
+  Widget _createVoucherWidget(Voucher voucher) {
+    return Card(
+      color: Colors.amber[600],
+      child: Center(child: Text(voucher.description)),
+    );
   }
 }
