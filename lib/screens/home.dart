38137264f81f91ab5db8e461f5ebd53a23cb4570
yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2022-06-27 00:21:52 +0800

    Fix dummy voucher data

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,21 +13,26 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final List<Voucher> _dummyVouchers = [
-    Voucher(code: "ABCD123456789", start: DateTime.now(),
+    Voucher(code: "IJKL123456789",
+        start: DateTime.now().subtract(const Duration(days: 29)),
+        end: DateTime.now().add(const Duration(days: 1)),
+        description: "Voucher Rp. 50000", status: VoucherStatus.active),
+    Voucher(code: "MNOP123456789",
+        start: DateTime.now().subtract(const Duration(days: 20)),
+        end: DateTime.now().add(const Duration(days: 10)),
+        description: "Voucher Rp. 50000", status: VoucherStatus.active),
+    Voucher(code: "QRST123456789",
+        start: DateTime.now().subtract(const Duration(days: 5)),
+        end: DateTime.now().add(const Duration(days: 25)),
+        description: "Voucher Rp. 50000", status: VoucherStatus.active),
+    Voucher(code: "ABCD123456789",
+        start: DateTime.now(),
         end: DateTime.now().add(const Duration(days: 30)),
         description: "Voucher Rp. 50000", status: VoucherStatus.active),
-    Voucher(code: "EFGH123456789", start: DateTime.now(),
-        end: DateTime.now().add(const Duration(days: 30)),
+    Voucher(code: "EFGH123456789",
+        start: DateTime.now().add(const Duration(days: 1)),
+        end: DateTime.now().add(const Duration(days: 31)),
         description: "Voucher Rp. 50000", status: VoucherStatus.inactive),
-    Voucher(code: "IJKL123456789", start: DateTime.now(),
-        end: DateTime.now().add(const Duration(days: 30)),
-        description: "Voucher Rp. 50000", status: VoucherStatus.used),
-    Voucher(code: "MNOP123456789", start: DateTime.now(),
-        end: DateTime.now().add(const Duration(days: 30)),
-        description: "Voucher Rp. 50000", status: VoucherStatus.expired),
-    Voucher(code: "QRST123456789", start: DateTime.now(),
-        end: DateTime.now().add(const Duration(days: 30)),
-        description: "Voucher Rp. 50000", status: VoucherStatus.active),
   ];
 
   late Future<void> _initVoucherData;
@@ -52,16 +57,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Future<List<Voucher>> _generateDummyVouchers() async {
     return Future.delayed(const Duration(seconds: 2), () {
-      List<Voucher> vouchers = [];
-
-      var rng = Random();
-      final totalDummyVouchers = rng.nextInt(5) + 1;
-      _dummyVouchers.shuffle(rng);
-      for (var i = 0; i < totalDummyVouchers; i++) {
-        vouchers.add(_dummyVouchers[i]);
-      }
-
-      return vouchers;
+      return _dummyVouchers;
     });
   }
 
