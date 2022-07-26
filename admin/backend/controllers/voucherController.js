aff5b866b25aff305c63ca29c7fafc9a7890761e
yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2022-07-26 22:59:30 +0800

    Separate database to its own file

--- admin/backend/controllers/voucherController.js
+++ admin/backend/controllers/voucherController.js
@@ -1,14 +1,6 @@
-// TODO: Find a better way to connect to postgres database
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'sugoi',
-  password: 'password',
-  port: 5432,
-})
+const db = require('../db')
 
 exports.voucher_list = async (req, res) => {
-  const { rows } = await pool.query('SELECT * FROM vouchers')
+  const { rows } = await db.query('SELECT * FROM vouchers')
   res.status(200).json(rows)
 }
