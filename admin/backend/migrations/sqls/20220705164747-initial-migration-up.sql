274b13650962c360717dbd012763e6a668e16d03
yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2022-10-02 23:59:27 +0800

    Use random uuid for customers' id

--- admin/backend/migrations/sqls/20220705164747-initial-migration-up.sql
+++ admin/backend/migrations/sqls/20220705164747-initial-migration-up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE customers (
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   email VARCHAR(100) UNIQUE,
   phone VARCHAR(30) NOT NULL UNIQUE
@@ -41,7 +41,7 @@ CREATE TABLE vouchers (
   status VARCHAR(30) NOT NULL,
   branch VARCHAR(100) NOT NULL,
 
-  customer_id INT NOT NULL,
+  customer_id UUID NOT NULL,
   CONSTRAINT fk_customer
     FOREIGN KEY(customer_id)
       REFERENCES customers(id),
@@ -69,9 +69,11 @@ CREATE INDEX vouchers_issue_receipt_id_fkey ON vouchers(issue_receipt_id);
 CREATE INDEX vouchers_claim_receipt_id_fkey ON vouchers(claim_receipt_id);
 
 /* TODO: Remove seed data before production */
+INSERT INTO customers (id, name, email, phone)
+  VALUES ('a35bc903-bb03-4a6a-a3ea-94d6135d603c', 'Jerry', 'jerry@example.com', '08981111222');
+
 INSERT INTO customers (name, email, phone)
-  VALUES ('Jerry', 'jerry@example.com', '08981111222'),
-  ('George', 'george@example.com', '11112222333');
+  VALUES ('George', 'george@example.com', '11112222333');
 
 INSERT INTO admins (username, hash_password, branch, role)
   VALUES ('MakassarAdmin', '12345678', 'Makassar', 'Admin');
@@ -82,9 +84,8 @@ INSERT INTO receipts(id, price, type, admin_id, created_at)
   ('RECEIPTC', 50000.00, 'Issue', 1, '11-06-2022 09:23:54+07');
 
 INSERT INTO vouchers(code, start_date, end_date, status, branch, customer_id, issue_receipt_id)
-  VALUES (1, '12-09-202 10:23:54+07', '12-10-2022 10:23:54+07', 'Active', 'Makassar', 1, 'RECEIPTA'),
-  (2, '11-06-2022 10:23:54+07', '11-07-2022 10:23:54+07', 'Expired', 'Makassar', 1, 'RECEIPTC');
+  VALUES (1, '12-09-202 10:23:54+07', '12-10-2022 10:23:54+07', 'Active', 'Makassar', 'a35bc903-bb03-4a6a-a3ea-94d6135d603c', 'RECEIPTA'),
+  (2, '11-06-2022 10:23:54+07', '11-07-2022 10:23:54+07', 'Expired', 'Makassar', 'a35bc903-bb03-4a6a-a3ea-94d6135d603c', 'RECEIPTC');
 
-INSERT INTO vouchers(code, start_date, end_date, claimed_date, status, branch, customer_id,
-  issue_receipt_id, claim_receipt_id)
-  VALUES (3, '11-09-2022 10:23:54+07', '11-10-2022 10:23:54+07', '12-07-2022 10:23:54+07', 'Used', 'Makassar', 1, 'RECEIPTA', 'RECEIPTB');
+INSERT INTO vouchers(code, start_date, end_date, claimed_date, status, branch, customer_id, issue_receipt_id, claim_receipt_id)
+  VALUES (3, '11-09-2022 10:23:54+07', '11-10-2022 10:23:54+07', '12-07-2022 10:23:54+07', 'Used', 'Makassar', 'a35bc903-bb03-4a6a-a3ea-94d6135d603c', 'RECEIPTA', 'RECEIPTB');
