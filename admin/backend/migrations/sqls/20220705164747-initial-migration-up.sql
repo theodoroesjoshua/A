fa5a40e1dd853d543b73c43646c96199c6c51e55
yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2022-10-02 22:59:05 +0800

    Add vouchers relation to receipt

--- admin/backend/migrations/sqls/20220705164747-initial-migration-up.sql
+++ admin/backend/migrations/sqls/20220705164747-initial-migration-up.sql
@@ -43,21 +43,15 @@ CREATE TABLE vouchers (
 
   PRIMARY KEY(customer_id, code),
 
-  /* Voucher Issuance Properties */
-  issuer_id INT NOT NULL,
-  CONSTRAINT fk_admin_issuer
-    FOREIGN KEY(issuer_id)
-      REFERENCES admins(id),
-  issue_receipt_id VARCHAR(30),
-  issue_receipt_price DOUBLE PRECISION,
+  issue_receipt_id VARCHAR(30) NOT NULL,
+  CONSTRAINT fk_issue_receipt
+    FOREIGN KEY(issue_receipt_id)
+      REFERENCES receipts(id),
 
-  /* Voucher Claiming Properties */
-  claimer_id INT,
-  CONSTRAINT fk_admin_claimer
-    FOREIGN KEY(claimer_id)
-      REFERENCES admins(id),
   claim_receipt_id VARCHAR(30),
-  claim_receipt_price DOUBLE PRECISION
+  CONSTRAINT fk_claim_receipt
+    FOREIGN KEY(claim_receipt_id)
+      REFERENCES receipts(id)
 );
 
 CREATE INDEX start_date_idx ON vouchers(start_date);
@@ -65,10 +59,8 @@ CREATE INDEX end_date_idx ON vouchers(end_date);
 CREATE INDEX status_idx ON vouchers(status);
 CREATE INDEX branch_idx ON vouchers(branch);
 CREATE INDEX customer_id_fkey ON vouchers(customer_id);
-CREATE INDEX issuer_id_fkey ON vouchers(issuer_id);
-CREATE INDEX issue_receipt_id_idx ON vouchers(issue_receipt_id);
-CREATE INDEX claimer_id_fkey ON vouchers(claimer_id);
-CREATE INDEX claim_receipt_id_idx ON vouchers(claim_receipt_id);
+CREATE INDEX issue_receipt_id_fkey ON vouchers(issue_receipt_id);
+CREATE INDEX claim_receipt_id_fkey ON vouchers(claim_receipt_id);
 
 /* TODO: Remove seed data before production */
 INSERT INTO customers (name, email, phone)
@@ -78,13 +70,15 @@ INSERT INTO customers (name, email, phone)
 INSERT INTO admins (username, hash_password, branch, role)
   VALUES ('MakassarAdmin', '12345678', 'Makassar', 'Admin');
 
-INSERT INTO vouchers(code, start_date, end_date, status, branch, customer_id, issuer_id,
-  issue_receipt_id, issue_receipt_price)
-  VALUES (1, '12-07-2022', '12-08-2022', 'Active', 'Makassar', 1, 1, 'RECEIPTA', 100000);
+INSERT INTO receipts(id, price, type, admin_id)
+  VALUES ('RECEIPTA', 100000.00, 'Issue', 1),
+  ('RECEIPTB', 100000.00, 'Claim', 1),
+  ('RECEIPTC', 50000.00, 'Issue', 1);
 
-INSERT INTO vouchers(code, start_date, end_date, claimed_date, status, branch, customer_id, issuer_id,
-  issue_receipt_id, issue_receipt_price, claimer_id, claim_receipt_id, claim_receipt_price)
-  VALUES (2, '11-07-2022', '11-08-2022', '12-07-2022', 'Used', 'Makassar', 1, 1, 'RECEIPTB', 100000,
-    1, 'RECEIPTC', 100000),
-  (3, '11-06-2022', '11-07-2022', NULL, 'Expired', 'Makassar', 1, 1, 'RECEIPTD', 100000,
-    1, 'RECEIPTE', 100000);
+INSERT INTO vouchers(code, start_date, end_date, status, branch, customer_id, issue_receipt_id)
+  VALUES (1, '12-09-2022', '12-10-2022', 'Active', 'Makassar', 1, 'RECEIPTA'),
+  (2, '11-06-2022', '11-07-2022', 'Expired', 'Makassar', 1, 'RECEIPTC');
+
+INSERT INTO vouchers(code, start_date, end_date, claimed_date, status, branch, customer_id,
+  issue_receipt_id, claim_receipt_id)
+  VALUES (3, '11-09-2022', '11-10-2022', '12-07-2022', 'Used', 'Makassar', 1, 'RECEIPTA', 'RECEIPTB');
