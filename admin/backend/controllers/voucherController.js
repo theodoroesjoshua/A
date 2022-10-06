05d1311307909466fa3b18a5c9b217443ed35943
yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2022-10-06 22:48:51 +0800

    Fetch relational database for vouchers

--- admin/backend/controllers/voucherController.js
+++ admin/backend/controllers/voucherController.js
@@ -1,6 +1,16 @@
 const db = require('@db')
 
 exports.voucher_list = async (req, res) => {
-  const { rows } = await db.query('SELECT * FROM vouchers')
+  const all_vouchers_query = `
+  SELECT code, start_date, end_date, claimed_date, status, branch, customer_id,
+  issue_receipt_id, issuer.price AS issuer_price, issuer.admin_id AS issuer_admin_id, issuer.created_at AS issue_date,
+  claim_receipt_id, claimer.price AS claimer_price, claimer.admin_id AS claimer_admin_id, claimer.created_at AS claim_date
+  FROM vouchers
+  LEFT JOIN receipts As issuer ON issue_receipt_id=issuer.id
+  LEFT JOIN receipts As claimer ON claim_receipt_id=claimer.id
+  ORDER BY start_date DESC;`
+
+  const { rows } = await db.query(all_vouchers_query)
+
   res.status(200).json(rows)
 }
