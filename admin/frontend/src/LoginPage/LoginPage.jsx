1c9b4eb75143f66b633d428f4a9c129736432333
Joshua <theodoroesjoshua@gmail.com>
2022-10-16 13:58:50 +0200

    Adjust LoginPage to remove warnings.

--- admin/frontend/src/LoginPage/LoginPage.jsx
+++ admin/frontend/src/LoginPage/LoginPage.jsx
@@ -6,10 +6,6 @@ import { authenticationService } from '../_services';
 import { withRouter } from '../_helpers';
 
 class LoginPageComponent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     // redirect to home if already logged in
     if (authenticationService.currentUserValue) {
@@ -24,49 +20,49 @@ class LoginPageComponent extends React.Component {
   render() {
     return (
       <div>
-          <h2>Login</h2>
-          <Formik
-              initialValues={{
-                  username: '',
-                  password: ''
-              }}
-              validationSchema={Yup.object().shape({
-                  username: Yup.string().required('Username is required'),
-                  password: Yup.string().required('Password is required')
-              })}
-              onSubmit={({ username, password }, { setStatus, setSubmitting }) => {
-                  setStatus()
-                  authenticationService.login(username, password)
-                      .then(() => {
-                        this.redirect_to_home_page()
-                      })
-                      .catch((error) => {
-                        setSubmitting(false);
-                        setStatus(error.response.data.msg);
-                      });
-                  }}
-              render={({ errors, status, touched, isSubmitting }) => (
-                  <Form>
-                      <div className="form-group">
-                          <label htmlFor="username">Username</label>
-                          <Field name="username" type="text" className={'form-control' + (errors.username && touched.username ? ' is-invalid' : '')} />
-                          <ErrorMessage name="username" component="div" className="invalid-feedback" />
-                      </div>
-                      <div className="form-group">
-                          <label htmlFor="password">Password</label>
-                          <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
-                          <ErrorMessage name="password" component="div" className="invalid-feedback" />
-                      </div>
-                      <div className="form-group">
-                          <button type="submit" className="btn btn-primary" disabled={isSubmitting}>Login</button>
-                          {isSubmitting}
-                      </div>
-                      {status &&
-                          <div className={'alert alert-danger'}>{status}</div>
-                      }
-                  </Form>
-              )}
-          />
+        <h2>Login</h2>
+        <Formik
+          initialValues={{
+            username: '',
+            password: ''
+          }}
+          validationSchema={Yup.object().shape({
+            username: Yup.string().required('Username is required'),
+            password: Yup.string().required('Password is required')
+          })}
+          onSubmit={({ username, password }, { setStatus, setSubmitting }) => {
+            setStatus()
+            authenticationService.login(username, password)
+              .then(() => {
+                this.redirect_to_home_page()
+              })
+              .catch((error) => {
+                setSubmitting(false);
+                setStatus(error.response.data.msg);
+              });
+          }}>
+          {({ errors, status, touched, isSubmitting }) => (
+            <Form>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <Field name="username" type="text" className={'form-control' + (errors.username && touched.username ? ' is-invalid' : '')} />
+                <ErrorMessage name="username" component="div" className="invalid-feedback" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
+                <ErrorMessage name="password" component="div" className="invalid-feedback" />
+              </div>
+              <div className="form-group">
+                <button type="submit" className="btn btn-primary" disabled={isSubmitting}>Login</button>
+                {isSubmitting}
+              </div>
+              {status &&
+                <div className={'alert alert-danger'}>{status}</div>
+              }
+            </Form>
+          )}
+        </Formik>
       </div>
     )
   }
